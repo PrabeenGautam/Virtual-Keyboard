f2eb7aadcab9d28e1c9b83508bca514b65fd971b
PrabinGautam <prabeen122@gmail.com>
2022-11-19 13:22:54 +0545

    Hover State Added

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+const numIndicator = document.querySelector(".num");
+const capsIndicator = document.querySelector(".caps");
+const scrollIndicator = document.querySelector(".scroll");
+
+function changeHover(key) {
+  const element = this.document.querySelector(`#${key}`);
+  if (element) {
+    element.classList.add("active");
+    this.setTimeout(() => element.classList.remove("active"), 200);
+  }
+}
+
 window.addEventListener("keydown", function (e) {
   e.preventDefault();
   const pressedKey = e.key.toLowerCase();
-  const element = this.document.querySelector(`#${pressedKey}`);
-  element.classList.add("active");
-  this.setTimeout(() => element.classList.remove("active"), 100);
+  console.log(pressedKey);
+  changeHover(pressedKey);
+});
+
+window.addEventListener("keyup", function (e) {
+  e.preventDefault();
+  if (e.key.toLowerCase() === "printscreen") {
+    changeHover(e.key.toLowerCase());
+  }
 });
